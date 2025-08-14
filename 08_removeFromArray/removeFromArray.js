e629bd7dc543f33b14357f65ec777d61461270c0
Derek <derekdvalerio@gmail.com>
2025-08-14 11:30:55 +0800

    COMPLETED: 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    // get the length of the arguments
+    let argLength = arguments.length;
+    // FOR EACH arg except the FIRST ONE
+    for (let i = 1; i < argLength; i++) {
+        // REMOVE FROM THE array
+        arr = arr.filter(item => item !== arguments[i]);
+    }      
+    // return the new array
+    return arr;
 };
 
 // Do not edit below this line
