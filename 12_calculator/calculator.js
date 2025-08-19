d4f86d80f916d69c6da85d8247109d71f5cc04a9
Derek <derekdvalerio@gmail.com>
2025-08-19 21:35:33 +0800

    FINISHED: 12_calculator AND 13_palindromes

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,20 @@
-const add = function() {
-	
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const sum = (arr) => arr.reduce((acc, current) => acc + current, 0);
+
+
+const multiply = (a) => a.reduce((acc, current) => acc * current, 1);
+
+const power = (base, exponent) => {
+  let arr = Array(exponent).fill(base);
+  return arr.reduce((acc, current) => acc * current, 1);
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = (a) =>
+  Array.from({ length: a }, (_, i) => a - i)
+    .reduce((acc, current) => acc * current, 1);
 
 // Do not edit below this line
 module.exports = {
