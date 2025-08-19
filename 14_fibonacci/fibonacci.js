8e0b5943d0a9dab6ca7505320c098a627c0b0fb6
Derek <derekdvalerio@gmail.com>
2025-08-19 21:54:50 +0800

    Finished: 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let nth = parseInt(n);
+    if (nth === 0) return 0;
+    if (nth < 0) return "OOPS";
+    if (nth === 1) return 1;
+
+
+    // create an array of length n, then use reduce to update the accumulator [a, b] 
+    return Array.from({length: n}).reduce(([a, b]) => [b, a + b], [0, 1])[0];
 
 };
 
