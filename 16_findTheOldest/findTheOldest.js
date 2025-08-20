dde09d26368d4a6583c05250ed7ac3901f78e13f
Derek <derekdvalerio@gmail.com>
2025-08-20 16:50:15 +0800

    FINISHED: 15_getTheTitles and 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const getAge = (birth, death) => {
+    if(!death) {
+        death = new Date().getFullYear();
+        return death - birth;
+    }
+    return death - birth;
+}
 
+const findTheOldest = (people) => {
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        return currentAge > oldestAge ? current : oldest;
+    })
 };
 
 // Do not edit below this line
